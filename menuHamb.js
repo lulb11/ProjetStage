18d8fab3a3f8f20491c29c8f0855943ee63fd619
lulb11 <bordeslucas@gmail.com>
2023-04-05 11:36:52 +0200

    ajouter compteur

--- menuHamb.js
+++ menuHamb.js
@@ -1,10 +1,10 @@
-const btnHamb = document.getElementById('btnHamb')
-const navLinks = document.getElementById('navLinks')
+const btnHamb = document.getElementById('btnHamb');
+const navLinks = document.getElementById('navLinks');
 
 btnHamb.addEventListener('click', function() {
   if (navLinks.classList.contains('show'))
     navLinks.classList.remove('show')
   else
     navLinks.classList.add('show')
-})
+});
 
